4007b8dfdb4c19899252962ff7f690e5f7038c86
Raja Biswas <rajjjabiswas@gmail.com>
2024-08-18 22:59:13 +0800

    setup

--- setup.sh
+++ setup.sh
@@ -9,8 +9,12 @@ kaggle competitions download -c lmsys-chatbot-arena
 unzip lmsys-chatbot-arena.zip -d lmsys-chatbot-arena
 rm lmsys-chatbot-arena.zip
 
-kaggle datasets download -d conjuring92/lmsys-mix-v01
-unzip lmsys-mix-v01.zip -d ./lmsys-mix-v01
-rm lmsys-mix-v01.zip
+kaggle datasets download -d conjuring92/lmsys-mix-v110
+unzip lmsys-mix-v01.zip -d ./lmsys-mix-v110
+rm lmsys-mix-v110.zip
+
+kaggle datasets download -d conjuring92/lmsys-mix-v205
+unzip lmsys-mix-v01.zip -d ./lmsys-mix-v110
+rm lmsys-mix-v110.zip
 
 cd $hdir
